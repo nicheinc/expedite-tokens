88325511ced805b0714c1f203cc25db007a982ea
spencerkingman-niche <skingman@niche.com>
2019-10-08 10:21:47 -0400

    Tune colors; add positioning

--- gulpfile.js
+++ gulpfile.js
@@ -4,47 +4,39 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 
 const $ = gulpLoadPlugins();
 
-// theo.registerFormat('color-map.scss', require('./formats/color-map.scss.js'));
-// theo.registerFormat('sketchpalette', require('./formats/sketchpalette.js'));
-// theo.registerFormat('ase.json', require('./formats/ase.json.js'));
-// theo.registerFormat('_colors.less', require('./formats/_colors.less.js'));
+theo.registerFormat('ase.json', require('./formats/ase.json.js'));
+theo.registerFormat('sketchpalette', require('./formats/sketchpalette.js'));
 
 const colorFormats = [
-    // {transformType: 'android', formatType: 'android.xml'},
-    // {transformType: 'web', formatType: 'color-map.scss'},
-    // {transformType: 'web', formatType: 'sketchpalette'},
-    // {transformType: 'web', formatType: 'ase.json'},
     {transformType: 'web', formatType: 'less'},
+    {transformType: 'web', formatType: 'scss'},
+    {transformType: 'web', formatType: 'html'},
+    {transformType: 'web', formatType: 'json'},
+    {transformType: 'web', formatType: 'ase.json'},
+    {transformType: 'android', formatType: 'android.xml'},
+    {transformType: 'web', formatType: 'sketchpalette'},
 ];
 
-// Hack to ensure Sass maps are prefixed with `expedite-`
-// (Theo relies on the filename to name all Sass maps)
-// const addPrefix = {prefix: 'expedite-'};
-
-// const removePrefix = (gulpRenameOptions) => {
-//     gulpRenameOptions.basename = gulpRenameOptions.basename.replace(
-//         'expedite-',
-//         '',
-//     );
-//     return gulpRenameOptions;
-// };
+const positioningFormats = [
+    {transformType: 'web', formatType: 'less'},
+    {transformType: 'web', formatType: 'scss'},
+    {transformType: 'web', formatType: 'json'},
+];
   
 gulp.task('print $', () => {
     console.log($)
 })
 
-gulp.task('color-formats', (done) => {
-    colorFormats.map(({transformType, formatType}) => {
+gulp.task('positioning-formats', (done) => {
+    positioningFormats.map(({transformType, formatType}) => {
         return gulp
-            .src('tokens/colors.yml')
-            // .pipe($.rename(addPrefix))
+            .src('tokens/positioning.yml')
             .pipe(
                 $.theo({
                     transform: {type: transformType, includeMeta: true},
                     format: {type: formatType},
                 }),
             )
-            // .pipe($.rename(removePrefix))
             .on('error', (err) => {
                 throw new Error(err);
             })
@@ -52,3 +44,29 @@ gulp.task('color-formats', (done) => {
         });
     done();
 });
+
+gulp.task('color-formats', (done) => {
+    colorFormats.map(({transformType, formatType}) => {
+        return gulp
+            .src('tokens/colors.yml')
+            .pipe(
+                $.theo({
+                    transform: {type: transformType, includeMeta: true},
+                    format: {type: formatType},
+                }),
+            )
+            .on('error', (err) => {
+                throw new Error(err);
+            })
+            .pipe(gulp.dest('dist'))
+        });
+    done();
+});
+
+gulp.task(
+    'default',
+    gulp.series([
+      'positioning-formats',
+      'color-formats',
+    ]),
+  );

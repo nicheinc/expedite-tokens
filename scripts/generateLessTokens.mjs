97274f0286827bd5b75a66e29a22d7f4e4916403
Keith Murgic <kmurgic@niche.com>
2022-05-10 15:17:01 -0400

    Add breakpoints js and less files

--- scripts/generateLessTokens.mjs
+++ scripts/generateLessTokens.mjs
@@ -1,14 +1,16 @@
 import { colors } from '../dist/index.js'
 import { shadows } from '../dist/index.js'
+import { breakpoints } from '../dist/index.js'
 import path from 'path'
 import * as fs from 'fs'
 const __dirname = path.resolve()
 
-const writeJsObjectToLessFile = ({ jsObject, outPath }) => {
+const writeJsObjectToLessFile = ({ jsObject, outPath, transform }) => {
     const outFileLines = []
     outFileLines.push('// THIS FILE WAS AUTOMATICALLY GENERATED')
     Object.keys(jsObject).forEach(varName => {
-        outFileLines.push(`@${varName}: ${jsObject[varName]};`)
+        const value = transform ? transform(jsObject[varName]) : jsObject[varName]
+        outFileLines.push(`@${varName}: ${value};`)
     })
     const finalOutput = `${outFileLines.join('\n')}\n`
 
@@ -22,3 +24,11 @@ console.log('Finished generating .less version of colors.ts...')
 console.log('Starting to generate .less version of shadows.ts...')
 writeJsObjectToLessFile({ jsObject: shadows, outPath: `${__dirname}/dist/shadows/shadows.less` })
 console.log('Finished generating .less version of shadows.ts...')
+
+console.log('Starting to generate .less version of breakpoints.ts...')
+writeJsObjectToLessFile({
+    jsObject: breakpoints,
+    outPath: `${__dirname}/dist/breakpoints/breakpoints.less`,
+    transform: value => `~'only screen and (min-width: ${value})'`,
+})
+console.log('Finished generating .less version of breakpoints.ts...')

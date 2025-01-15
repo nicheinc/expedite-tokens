0b2be158fe89423727221b591a8d9e25c7ac57c6
Spencer Kingman <spencer.kingman@niche.com>
2025-01-15 11:38:09 -0500

    rename generate

--- scripts/generateLessTokens.js
+++ scripts/generateLessTokens.js
@@ -1,21 +1,19 @@
-import { colors } from '../dist/index.js'
-import { shadows } from '../dist/index.js'
-import { breakpoints } from '../dist/index.js'
-import path from 'path'
 import * as fs from 'fs'
-const __dirname = path.resolve()
+import { breakpoints, colors, shadows } from '../dist/index.js'
+import path from 'path'
 
-const writeJsObjectToLessFile = ({ jsObject, outPath, transform }) => {
-    const outFileLines = []
-    outFileLines.push('// THIS FILE WAS AUTOMATICALLY GENERATED')
-    Object.keys(jsObject).forEach(varName => {
-        const value = transform ? transform(jsObject[varName]) : jsObject[varName]
-        outFileLines.push(`@${varName}: ${value};`)
-    })
-    const finalOutput = `${outFileLines.join('\n')}\n`
+const __dirname = path.resolve(),
+    writeJsObjectToLessFile = ({ jsObject, outPath, transform }) => {
+        const outFileLines = []
+        outFileLines.push('// THIS FILE WAS AUTOMATICALLY GENERATED')
+        Object.keys(jsObject).forEach(varName => {
+            const value = transform ? transform(jsObject[varName]) : jsObject[varName]
+            outFileLines.push(`@${varName}: ${value};`)
+        })
+        const finalOutput = `${outFileLines.join('\n')}\n`
 
-    fs.writeFile(outPath, finalOutput, err => err && console.error(err))
-}
+        fs.writeFile(outPath, finalOutput, err => err && console.error(err))
+    }
 
 console.log('Starting to generate .less version of colors.ts...')
 writeJsObjectToLessFile({ jsObject: colors, outPath: `${__dirname}/dist/colors/colors.less` })

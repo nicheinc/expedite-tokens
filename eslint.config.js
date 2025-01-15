258f6fac3f4ada63149d484f2767571e0400375c
Spencer Kingman <spencer.kingman@niche.com>
2025-01-15 10:48:50 -0500

    create new eslint config

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import ESLintJS from '@eslint/js'
+import TypeScriptESLint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import progress from 'eslint-plugin-file-progress'
+import tsParser from '@typescript-eslint/parser'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+
+export default [
+    {
+        ignores: ['**/dist', '**/.gitignore'],
+    },
+    ESLintJS.configs.recommended,
+    ...TypeScriptESLint.configs.recommended,
+    progress.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        files: ['scripts/', 'tokens/'],
+    },
+    {
+        languageOptions: {
+            ecmaVersion: 2018,
+            globals: {
+                ...globals.node,
+            },
+            parser: tsParser,
+            parserOptions: {
+                allowImportExportEverywhere: true,
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                project: './tsconfig.json',
+            },
+            sourceType: 'module',
+        },
+        plugins: {
+            '@typescript-eslint': typescriptEslintPlugin,
+        },
+        rules: {
+            '@typescript-eslint/indent': 'off',
+        },
+    },
+]
